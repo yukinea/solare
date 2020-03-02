a86dc6f4674938e1202728b29e4ecd185131eea6
yuknea <yuki.931322@gmail.com>
2020-03-03 00:48:11 +0900

    update: devel サービスアカウントでカレンダー登録できる

--- develop/google_calendar/quickstart.py
+++ develop/google_calendar/quickstart.py
@@ -1,48 +1,24 @@
 from __future__ import print_function
 import datetime
-import pickle
 import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.events']
-
+GOOGLE_APPLICATION_CREDENTIALS='' # サービスアカウントのJSONファイルパス
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
-
+    c = Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
+    service = build('calendar', 'v3', credentials=c)
+    
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 
     calendar_id = 'primary'
     body = dict()
     body['summary'] = 'Test summary'
-    body['start'] = {'timeZone': 'Asia/Tokyo', 'dateTime': '2020-03-02T11:00:00+09:00'}
-    body['end'] = {'timeZone': 'Asia/Tokyo', 'dateTime': '2020-03-02T11:30:00+09:00'}
+    body['start'] = {'timeZone': 'Asia/Tokyo', 'dateTime': '2020-03-04T11:00:00+09:00'}
+    body['end'] = {'timeZone': 'Asia/Tokyo', 'dateTime': '2020-03-04T11:30:00+09:00'}
     body['location'] = 'Kobe'
     body['guestsCanSeeOtherGuests'] = False
     body['guestsCanInviteOthers'] = False
